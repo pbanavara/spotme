0b3b5dfe9686adc06fd9c3c137b8afc353e92ed9
Pradeep Banavara <pradeepbs@gmail.com>
2024-07-01 22:09:37 +0530

    Adding a production secret

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,8 @@ const nextConfig = {
         POSTGRES_URL: process.env.POSTGRES_URL,
         POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING,
         POSTGRES_URL_NO_SSL: process.env.POSTGRES_URL_NO_SSL,
-        POSGTRES_USER: process.env.POSGTRES_USER
+        POSGTRES_USER: process.env.POSGTRES_USER,
+        SECRET: process.env.SECRET
     },
     images: {
         domains: [
