7e5330e25a5a96ad3a40ae69d1cd84cf9234441f
Pradeep Banavara <pradeepbs@gmail.com>
2024-07-01 22:29:32 +0530

    Trying some CORS config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/',
+                headers: [
+                    { key: 'Access-Control-Allow-Credentials', value: 'true' },
+                    { key: 'Access-Control-Allow-Origin', value: 'https://spotme-xi.vercel.app' },
+                    { key: 'Access-Control-Allow-Methods', value: 'GET, DELETE, PATCH, POST, PUT' },
+                    { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+                ],
+            },
+        ];
+    },
     reactStrictMode: true,
     env: {
         POSTGRES_DATABASE: process.env.POSTGRES_DATABASE,
